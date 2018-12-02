89c02dba4e99216f0f33741d0bb793265a98e7e2
leszrom <romanowski.leszek@gmail.com>
2018-12-02 13:07:23 +0100

    Find all loans by user id

--- src/main/java/com/crud/library/repository/LoanRepository.java
+++ src/main/java/com/crud/library/repository/LoanRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface LoanRepository extends CrudRepository<Loan, Long> {
-
+    List<Loan> findAllByUserId(long id);
 }
