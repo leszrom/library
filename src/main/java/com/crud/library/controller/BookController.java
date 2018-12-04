26665e6d9e06f1d72d043d43e4d40492644c8e1b
leszrom <romanowski.leszek@gmail.com>
2018-12-04 13:25:17 +0100

    BookController

---
+++ src/main/java/com/crud/library/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.crud.library.controller;
+
+import com.crud.library.mapper.BookMapper;
+import com.crud.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/books")
+public class BookController {
+    private BookService bookService;
+    private BookMapper bookMapper;
+
+    @Autowired
+    public BookController(BookService bookService, BookMapper bookMapper) {
+        this.bookService = bookService;
+        this.bookMapper = bookMapper;
+    }
+}
