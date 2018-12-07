aada18af33b2f4e2b20fa3b8fbc37680a4bb330d
leszrom <romanowski.leszek@gmail.com>
2018-12-07 13:39:08 +0100

    UserMapper from Controller to Service

--- src/main/java/com/crud/library/controller/UserController.java
+++ src/main/java/com/crud/library/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.crud.library.controller;
 
 import com.crud.library.domain.dto.UserDto;
-import com.crud.library.mapper.UserMapper;
 import com.crud.library.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,35 +12,31 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
-    private static final String USER_DOES_NOT_EXIST = "The user with the given id does not exist";
     private final UserService userService;
-    private final UserMapper userMapper;
 
     @Autowired
-    public UserController(UserService userService, UserMapper userMapper) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
     public Long createUser(@RequestBody UserDto userDto) {
-        return userService.saveUser(userMapper.mapToUser(userDto)).getId();
+        return userService.saveUser(userDto).getId();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "{id}")
     public UserDto getUser(@PathVariable Long id) throws UserNotFoundException {
-        return userMapper.mapToUserDto(userService.getUserById(id)
-                .orElseThrow(() -> new UserNotFoundException(USER_DOES_NOT_EXIST)));
+        return userService.getUserById(id);
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<UserDto> getUsers() {
-        return userMapper.mapToUserDtoList(userService.getAllUsers());
+        return userService.getAllUsers();
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "{id}", consumes = APPLICATION_JSON_VALUE)
-    public UserDto changeUserDetails(@RequestBody UserDto userDto, @PathVariable Long id) {
-        return userMapper.mapToUserDto(userService.updateUser(userMapper.mapToUser(userDto)));
+    public UserDto changeUserDetails(@PathVariable Long id, @RequestBody UserDto userDto) {
+        return userService.updateUser(id, userDto);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
--- src/main/java/com/crud/library/service/UserService.java
+++ src/main/java/com/crud/library/service/UserService.java
@@ -1,44 +1,52 @@
 package com.crud.library.service;
 
+import com.crud.library.controller.UserNotFoundException;
 import com.crud.library.domain.User;
+import com.crud.library.domain.dto.UserDto;
+import com.crud.library.mapper.UserMapper;
 import com.crud.library.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
+    private static final String USER_DOES_NOT_EXIST = "The user with the given id does not exist";
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
-    public User saveUser(final User user) {
+    public UserDto saveUser(final UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
         user.setCreated(new Date());
-        return userRepository.save(user);
+        return userMapper.mapToUserDto(userRepository.save(user));
     }
 
-    public Optional<User> getUserById(final Long id) {
-        return userRepository.findById(id);
+    public UserDto getUserById(final Long id) throws UserNotFoundException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(USER_DOES_NOT_EXIST));
+        return userMapper.mapToUserDto(user);
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userMapper.mapToUserDtoList(userRepository.findAll());
     }
 
-    public User updateUser(final User user) {
-        if (userRepository.findById(user.getId()).isPresent()) {
-            Date created = userRepository.findById(user.getId()).get().getCreated();
+    public UserDto updateUser(final Long id, final UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
+        if (userRepository.findById(id).isPresent()) {
+            Date created = userRepository.findById(id).get().getCreated();
             user.setCreated(created);
         } else {
             user.setCreated(new Date());
         }
-        return userRepository.save(user);
+        return userMapper.mapToUserDto(userRepository.save(user));
     }
 
     public void deleteUserById(final Long id) {
