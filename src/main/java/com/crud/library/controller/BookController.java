6b5b6f15ee5ff722799d2bb6ee931157df17614b
leszrom <romanowski.leszek@gmail.com>
2018-12-09 05:33:59 +0100

    BookMapper from Controller to Service

--- src/main/java/com/crud/library/controller/BookController.java
+++ src/main/java/com/crud/library/controller/BookController.java
@@ -3,7 +3,6 @@ package com.crud.library.controller;
 import com.crud.library.domain.Book;
 import com.crud.library.domain.Volume;
 import com.crud.library.domain.dto.BookDto;
-import com.crud.library.mapper.BookMapper;
 import com.crud.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +13,19 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("v1/books")
 public class BookController {
     private BookService bookService;
-    private BookMapper bookMapper;
 
     @Autowired
-    public BookController(BookService bookService, BookMapper bookMapper) {
+    public BookController(BookService bookService) {
         this.bookService = bookService;
-        this.bookMapper = bookMapper;
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
     public Long createBook(@RequestBody BookDto bookDto) {
-        return bookService.saveBook(bookMapper.mapToBook(bookDto)).getId();
+        return bookService.saveBook(bookDto);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "{bookId}/volumes")
     public void addBookVolume(@PathVariable Long bookId) {
-        Book theBook = bookService.getBookById(bookId).get();
-        theBook.getVolumes().add(new Volume(theBook, false));
-        bookService.saveBook(theBook);
+        bookService.addVolume(bookId);
     }
 }
--- src/main/java/com/crud/library/service/BookService.java
+++ src/main/java/com/crud/library/service/BookService.java
@@ -1,6 +1,10 @@
 package com.crud.library.service;
 
+import com.crud.library.controller.BookNotFoundException;
 import com.crud.library.domain.Book;
+import com.crud.library.domain.Volume;
+import com.crud.library.domain.dto.BookDto;
+import com.crud.library.mapper.BookMapper;
 import com.crud.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,23 +15,34 @@ import java.util.Optional;
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Autowired
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, BookMapper bookMapper) {
         this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
     }
 
-    public Book saveBook(final Book book) {
-        return bookRepository.save(book);
+    public Long saveBook(final BookDto bookDto) {
+        Book book = bookMapper.mapToBook(bookDto);
+        return bookRepository.save(book).getId();
+    }
+
+    public Optional<Book> getBookById(final Long id) {
+        return bookRepository.findById(id);
     }
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-    public Optional<Book> getBookById(final Long id) {
-        return bookRepository.findById(id);
+    public void addVolume(final Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(BookNotFoundException::new);
+        book.getVolumes().add(new Volume(book, false));
+        bookRepository.save(book);
     }
+
+
     public void deleteBookById(final Long id) {
         bookRepository.deleteById(id);
     }
